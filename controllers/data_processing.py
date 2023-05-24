91b8cfa1417cc8fe018be1f300d53a7b8ec51ab6
dimeBrii <thiswasbrii@gmail.com>
2023-05-24 04:00:28 -0400

    agregando tablas mejoradas

--- controllers/data_processing.py
+++ controllers/data_processing.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 import pandas as pd
 
 
@@ -7,7 +8,12 @@ def total_participants(data):
     Args:
         data (data frame): data frame de pandas que tiene los participantes
     """
-    print(f"Total de participantes: {len(data)-1}")  # resto 1 por la cabecera
+    print()
+    # resto 1 por la cabecera e imprimo
+    print(f"Total de participantes: {len(data)-1}")
+    print("Presione enter para continuar...")
+    input()
+    print()
 
 
 def list_participants(data):
@@ -15,7 +21,13 @@ def list_participants(data):
     Args:
         data (data frame): data frame de pandas que tiene los participantes
     """
-    print(data.to_string())
+    print()
+    print("Lista de participantes:")
+    print(tabulate(data, headers='keys', tablefmt='psql', showindex=False))
+    print()
+    print("Presione enter para continuar...")
+    input()
+    print()
 
 
 def etatian_groups(data):
@@ -24,14 +36,27 @@ def etatian_groups(data):
     Args:
         data (data frame): data frame de pandas que tiene los participantes
     """
+    print()
     print("Cantidad de participantes por grupo etario")
-    print("Juniors: ", end="")
-    print(list(data[data["Edad"] <= 25].to_string()).count("\n"))
-    print("Seniors: ", end="")
-    print(list(data[(data["Edad"] > 25) & (
-        data["Edad"] <= 40)].to_string()).count("\n"))
-    print("Masters: ", end="")
-    print(list(data[data["Edad"] > 40].to_string()).count("\n"))
+
+    # Cuenta la cantidad de participantes por grupo etario
+    juniors = list(data[data["Edad"] <= 25].to_string()).count("\n")
+    seniors = list(data[(data["Edad"] > 25) & (
+        data["Edad"] <= 40)].to_string()).count("\n")
+    masters = list(data[data["Edad"] > 40].to_string()).count("\n")
+
+    # Imprime la tabla
+    print(f"+{'-'*12}+{'-'*12}+")
+    print(f"| {'Categoría':<10} | {'Cantidad':<10} |")
+    print(f"|{'-'*12}|{'-'*12}|")
+    print(f"| {'Juniors':<10} | {juniors:<10} |")
+    print(f"| {'Seniors':<10} | {seniors:<10} |")
+    print(f"| {'Masters':<10} | {masters:<10} |")
+    print(f"+{'-'*12}+{'-'*12}+")
+
+    print("Presione enter para continuar...")
+    input()
+    print()
 
 
 def gender_groups(data):
@@ -41,10 +66,23 @@ def gender_groups(data):
         data (data frame): data frame de pandas que tiene los participantes
     """
     print("Cantidad de participantes por sexo")
-    print("Masculino: ", end="")
-    print(list(data[data["Sexo"] == "M"].to_string()).count("\n"))
-    print("Femenino: ", end="")
-    print(list(data[data["Sexo"] == "F"].to_string()).count("\n"))
+
+    # Cuenta la cantidad de participantes por sexo
+    masculine_count = list(data[data["Sexo"] == "M"].to_string()).count("\n")
+    femenine_count = list(data[data["Sexo"] == "F"].to_string()).count("\n")
+
+    # Imprime la tabla
+    print(f"+{'-'*12}+{'-'*12}+")
+    print(f"| {'Sexo':<10} | {'Cantidad':<10} |")
+    print(f"|{'-'*12}|{'-'*12}|")
+    print(f"| {'Masculino':<10} | {masculine_count:<10} |")
+    print(f"| {'Femenino':<10} | {femenine_count:<10} |")
+    print(f"+{'-'*12}+{'-'*12}+")
+
+    print()
+    print("Presione enter para continuar...")
+    input()
+    print()
 
 
 def winners_by_eterian_group(data):
@@ -54,17 +92,33 @@ def winners_by_eterian_group(data):
         data (data frame): data frame de pandas que tiene los participantes
     """
     print("Ganadores por grupo etario")
-    print("Juniors: ")
-    print(data[data["Edad"] <= 25].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+
+    # Filtrar los datos por cada categoría y obtener el ganador
+    junior_winner = data[data["Edad"] <= 25].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+    senior_winner = data[(data["Edad"] > 25) & (data["Edad"] <= 40)].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+    master_winner = data[data["Edad"] > 40].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+
+    # Crear una nueva columna con la etiqueta correspondiente para cada ganador
+    junior_winner["Grupo Etario"] = "Junior"
+    senior_winner["Grupo Etario"] = "Senior"
+    master_winner["Grupo Etario"] = "Master"
+
+    # Concatenar los datos de los ganadores en un único DataFrame
+    winners = pd.concat([junior_winner, senior_winner, master_winner])
+
+    # Seleccionar las columnas en el orden deseado
+    winners = winners[["Grupo Etario", "CI", "1er Apellido", "2do Apellido",
+                       "Nombre", "Edad", "Sexo", "Horas", "Minutos", "Segundos"]]
+
+    # Imprimir la tabla
+    print(tabulate(winners, headers='keys', tablefmt='psql', showindex=False))
     print()
-    print("Seniors: ")
-    print(data[(data["Edad"] > 25) & (
-        data["Edad"] <= 40)].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print("Presione enter para continuar...")
+    input()
     print()
-    print("Masters: ")
-    print(data[data["Edad"] > 40].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
 
 
 def winners_by_gender(data):
@@ -74,13 +128,31 @@ def winners_by_gender(data):
         data (data frame): data frame de pandas que tiene los participantes
     """
     print("Ganadores por sexo")
-    print("Masculino: ")
-    print(data[data["Sexo"] == "M"].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+
+    # Filtrar los datos por cada sexo y obtener el ganador
+    masculine_winner = data[data["Sexo"] == "M"].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+    femenine_winner = data[data["Sexo"] == "F"].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+
+    # Crear una nueva columna con la etiqueta correspondiente para cada ganador
+    masculine_winner["Genero"] = "Masculino"
+    femenine_winner["Genero"] = "Femenino"
+
+    # Concatenar los datos de los ganadores en un único DataFrame
+    winners = pd.concat([masculine_winner, femenine_winner])
+
+    # Seleccionar las columnas en el orden deseado
+    winners = winners[["Genero", "CI", "1er Apellido", "2do Apellido",
+                       "Nombre", "Edad", "Sexo", "Horas", "Minutos", "Segundos"]]
+
+    # Imprimir la tabla
+
+    print(tabulate(winners, headers='keys', tablefmt='psql', showindex=False))
     print()
-    print("Femenino: ")
-    print(data[data["Sexo"] == "F"].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print("Presione enter para continuar...")
+    input()
+    print()
 
 
 def winner_by_gender_and_etarian_group(data):
@@ -91,34 +163,55 @@ def winner_by_gender_and_etarian_group(data):
         data (data frame): data frame de pandas que tiene los participantes
     """
 
-    print("Ganador Masculino Junior")
-    print(data[(data["Edad"] <= 25) & (data["Sexo"] == "M")].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
-    print()
+    print("Ganadores por sexo y grupo etario")
 
-    print("Ganador Masculino Senior")
-    print(data[(data["Edad"] > 25) & (data["Edad"] <= 40) & (
-        data["Sexo"] == "M")].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
-    print()
+    masculine_junior_winner = data[(data["Edad"] <= 25) & (data["Sexo"] == "M")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
 
-    print("Ganador Masculino Master")
-    print(data[(data["Edad"] > 40) & (data["Sexo"] == "M")].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
-    print()
+    masculine_senior_winner = data[(data["Edad"] > 25) & (data["Edad"] <= 40) & (
+        data["Sexo"] == "M")].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1)
 
-    print("Ganador Femenino Junior")
-    print(data[(data["Edad"] <= 25) & (data["Sexo"] == "F")].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
-    print()
+    masculine_master_winner = data[(data["Edad"] > 40) & (data["Sexo"] == "M")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
 
-    print("Ganador Femenino Senior")
-    print(data[(data["Edad"] > 25) & (data["Edad"] <= 40) & (
-        data["Sexo"] == "F")].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
-    print()
+    femenine_junior_winner = data[(data["Edad"] <= 25) & (data["Sexo"] == "F")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
 
-    print("Ganador Femenino Master")
-    print(data[(data["Edad"] > 40) & (data["Sexo"] == "F")].sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    femenine_senior_winner = data[(data["Edad"] > 25) & (data["Edad"] <= 40) & (
+        data["Sexo"] == "F")].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1)
+
+    femenine_master_winner = data[(data["Edad"] > 40) & (data["Sexo"] == "F")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+
+    # Crear una nueva columna con la etiqueta correspondiente para cada ganador
+    masculine_junior_winner["Grupo Etario"] = "Junior"
+    masculine_junior_winner["Genero"] = "Masculino"
+    masculine_senior_winner["Grupo Etario"] = "Senior"
+    masculine_senior_winner["Genero"] = "Masculino"
+    masculine_master_winner["Grupo Etario"] = "Master"
+    masculine_master_winner["Genero"] = "Masculino"
+    femenine_junior_winner["Grupo Etario"] = "Junior"
+    femenine_junior_winner["Genero"] = "Femenino"
+    femenine_senior_winner["Grupo Etario"] = "Senior"
+    femenine_senior_winner["Genero"] = "Femenino"
+    femenine_master_winner["Grupo Etario"] = "Master"
+    femenine_master_winner["Genero"] = "Femenino"
+
+    # Concatenar los datos de los ganadores en un único DataFrame
+    winners = pd.concat([masculine_junior_winner, masculine_senior_winner, masculine_master_winner,
+                        femenine_junior_winner, femenine_senior_winner, femenine_master_winner])
+
+    # Seleccionar las columnas en el orden deseado
+    winners = winners[["Grupo Etario", "Genero", "CI", "1er Apellido",
+                       "2do Apellido", "Nombre", "Edad", "Sexo", "Horas", "Minutos", "Segundos"]]
+
+    # Imprimir la tabla
+
+    print(tabulate(winners, headers='keys', tablefmt='psql', showindex=False))
+
+    print()
+    print("Presione enter para continuar...")
+    input()
     print()
 
 
@@ -131,9 +224,15 @@ def general_winner(data):
 
     """
 
-    print("Ganador General")
-    print(data.sort_values(
-        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print("Ganador General:")
+   # Obtener el ganador general
+    general_winner = data.sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1)
+    winner_dict = general_winner.to_dict('records')[0]
+    print(f"{winner_dict['1er Apellido']} {winner_dict['2do Apellido']} {winner_dict['Nombre']} ({winner_dict['Edad']}, {winner_dict['Sexo']}) - tiempo de culminacion: {winner_dict['Horas']}:{winner_dict['Minutos']}:{winner_dict['Segundos']}")
+    print()
+    print("Presione enter para continuar...")
+    input()
     print()
 
 
@@ -145,7 +244,7 @@ def generate_histomgram(data):
         data (data frame): data frame de pandas que tiene los participantes
     """
 
-    print("Histograma de grupos etarios")
+    print("Histograma de grupos etarios:")
     print()
     juniors = list(data[data["Edad"] <= 25].to_string()).count("\n")
     print(f"Junior ({juniors}):\t|{'*'*juniors} ")  # f string
@@ -156,4 +255,7 @@ def generate_histomgram(data):
 
     masters = list(data[data["Edad"] > 40].to_string()).count("\n")
     print(f"Master ({masters}):\t|{'*'*masters} ")
+    print()
+    print("Presione enter para continuar...")
+    input()
     print()

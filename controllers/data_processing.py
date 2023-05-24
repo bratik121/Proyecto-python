39cce844f468e14a5f24ce011a9da7e8b26f2aab
dimeBrii <thiswasbrii@gmail.com>
2023-05-24 01:58:19 -0400

    anadiendo funcionaliadesa a los controlladores y mejorando menu

--- controllers/data_processing.py
+++ controllers/data_processing.py
@@ -1,19 +1,159 @@
 import pandas as pd
 
 
-
-
 def total_participants(data):
     """Imprime la cantidad de participantes
 
     Args:
-        data (list): lista de los participantes
+        data (data frame): data frame de pandas que tiene los participantes
     """
-    print(f"Total de participantes: {len(data)-1}") #resto 1 por la cabecera
+    print(f"Total de participantes: {len(data)-1}")  # resto 1 por la cabecera
+
 
 def list_participants(data):
     """Imprime la lista de participantes
     Args:
-        data (list): lista de los participantes
+        data (data frame): data frame de pandas que tiene los participantes
     """
     print(data.to_string())
+
+
+def etatian_groups(data):
+    """Imprime la cantidad de participantes por grupo etario
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+    """
+    print("Cantidad de participantes por grupo etario")
+    print("Juniors: ", end="")
+    print(list(data[data["Edad"] <= 25].to_string()).count("\n"))
+    print("Seniors: ", end="")
+    print(list(data[(data["Edad"] > 25) & (
+        data["Edad"] <= 40)].to_string()).count("\n"))
+    print("Masters: ", end="")
+    print(list(data[data["Edad"] > 40].to_string()).count("\n"))
+
+
+def gender_groups(data):
+    """Imprime la cantidad de participantes por sexo
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+    """
+    print("Cantidad de participantes por sexo")
+    print("Masculino: ", end="")
+    print(list(data[data["Sexo"] == "M"].to_string()).count("\n"))
+    print("Femenino: ", end="")
+    print(list(data[data["Sexo"] == "F"].to_string()).count("\n"))
+
+
+def winners_by_eterian_group(data):
+    """Imprime los ganadores por grupo etario
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+    """
+    print("Ganadores por grupo etario")
+    print("Juniors: ")
+    print(data[data["Edad"] <= 25].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+    print("Seniors: ")
+    print(data[(data["Edad"] > 25) & (
+        data["Edad"] <= 40)].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+    print("Masters: ")
+    print(data[data["Edad"] > 40].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+
+
+def winners_by_gender(data):
+    """Imprime los ganadores por sexo
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+    """
+    print("Ganadores por sexo")
+    print("Masculino: ")
+    print(data[data["Sexo"] == "M"].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+    print("Femenino: ")
+    print(data[data["Sexo"] == "F"].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+
+
+def winner_by_gender_and_etarian_group(data):
+    """
+    imprime los gandores por sexo y grupo etario
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+    """
+
+    print("Ganador Masculino Junior")
+    print(data[(data["Edad"] <= 25) & (data["Sexo"] == "M")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+    print("Ganador Masculino Senior")
+    print(data[(data["Edad"] > 25) & (data["Edad"] <= 40) & (
+        data["Sexo"] == "M")].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+    print("Ganador Masculino Master")
+    print(data[(data["Edad"] > 40) & (data["Sexo"] == "M")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+    print("Ganador Femenino Junior")
+    print(data[(data["Edad"] <= 25) & (data["Sexo"] == "F")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+    print("Ganador Femenino Senior")
+    print(data[(data["Edad"] > 25) & (data["Edad"] <= 40) & (
+        data["Sexo"] == "F")].sort_values(by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+    print("Ganador Femenino Master")
+    print(data[(data["Edad"] > 40) & (data["Sexo"] == "F")].sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+
+def general_winner(data):
+    """
+    Imprime el ganador general
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+
+    """
+
+    print("Ganador General")
+    print(data.sort_values(
+        by=["Horas", "Minutos", "Segundos"]).head(1).to_string())
+    print()
+
+
+def generate_histomgram(data):
+    """
+    Genera un histograma por cada grupo etario
+
+    Args:
+        data (data frame): data frame de pandas que tiene los participantes
+    """
+
+    print("Histograma de grupos etarios")
+    print()
+    juniors = list(data[data["Edad"] <= 25].to_string()).count("\n")
+    print(f"Junior ({juniors}):\t|{'*'*juniors} ")  # f string
+
+    seniors = list(data[(data["Edad"] > 25) & (
+        data["Edad"] <= 40)].to_string()).count("\n")
+    print(f"Senior ({seniors}):\t|{'*'*seniors} ")
+
+    masters = list(data[data["Edad"] > 40].to_string()).count("\n")
+    print(f"Master ({masters}):\t|{'*'*masters} ")
+    print()
--- pages/menu.py
+++ pages/menu.py
@@ -3,48 +3,95 @@ from controllers import data_acquisition
 from controllers import data_processing
 
 
-
 def start():
     option1 = 0
     data = None
-    while(option1 != 3):
+    # Menu principal
+    while (option1 != 3):
         print("Bienvenido a la competicion!")
         print("1. Archivo")
         print("2. Acciones")
         print("3. Salir")
-        option1 = int(input())
-
-        if(option1 == 1):
+        try:
+            option1 = int(input())
+        except ValueError:
+            print("****Debe introducir un numero****")
+            print()
+            continue
+        # Menu de archivos
+        if (option1 == 1):
             option2 = 0
-            while(option2 != 2):
+            while (option2 != 10):
                 print("1. Cargar archivo")
-                print("2. Salir")
-                option2 = int(input())
-                if(option2 == 1):
-                    filename=""
-                    while(os.path.isfile("data/"+filename) == False):
+                print("2. Volver")
+                print("3. Salir")
+                try:
+                    option2 = int(input())
+                except ValueError:
+                    print("****Debe introducir un numero****")
+                    print()
+                    continue
+                if (option2 == 1):
+                    filename = ""
+                    while (os.path.isfile("data/"+filename) == False):
                         print("Ingrese el nombre de un archivo valido: ")
                         filename = input()
                     data = data_acquisition.read_file(filename)
-
-        elif(option1 == 2):
+                elif (option2 == 2):
+                    print("Volviendo al menu principal")
+                    print()
+                elif (option2 == 3):
+                    print("Gracias por usar el programa!")
+                    option1 = 3
+                    option2 = 10
+                else:
+                    print("Ingrese una opcion valida")
+        elif (option1 == 2):
             option2 = 0
-            while(option2 != 2):
+            while (option2 != 10):
                 print("1. Cantidad total de participantes")
                 print("2. Lista con la totalidad de los participantes")
                 print("3. Cantidad de participantes por grupo etario")
                 print("4. Cantidad de participantes por sexo")
                 print("5. Ganadores por grupo etario")
-                print("5. Ganadores por sexo")
-                print("6. Ganadores por Grupo Etario y sexo")
-                print("7. Ganador General")
-                print("8. Historigrama de Participantes por grupo Etario")
-                print("9. Promedio de Tiempo por grupo Etario y Sexo")
+                print("6. Ganadores por sexo")
+                print("7. Ganadores por Grupo Etario y sexo")
+                print("8. Ganador General")
+                print("9. Historigrama de Participantes por grupo Etario")
                 print("10. Volver")
                 print("11. Salir")
-
-                option2 = int(input())
-                if(option2 == 1):
+                try:
+                    option2 = int(input())
+                except ValueError:
+                    print("****Debe introducir un numero****")
+                    print()
+                    continue
+                if (option2 == 1):
                     data_processing.total_participants(data)
-                elif(option2 == 2):
+                elif (option2 == 2):
                     data_processing.list_participants(data)
+                elif (option2 == 3):
+                    data_processing.etatian_groups(data)
+                elif (option2 == 4):
+                    data_processing.gender_groups(data)
+                elif (option2 == 5):
+                    data_processing.winners_by_eterian_group(data)
+                elif (option2 == 6):
+                    data_processing.winners_by_gender(data)
+                elif (option2 == 7):
+                    data_processing.winner_by_gender_and_etarian_group(data)
+                elif (option2 == 8):
+                    data_processing.general_winner(data)
+                elif (option2 == 9):
+                    data_processing.generate_histomgram(data)
+                elif (option2 == 10):
+                    print("Volviendo al menu principal")
+                    print()
+                elif (option2 == 11):
+                    print("Gracias por usar el programa!")
+                    option1 = 3
+                    option2 = 10
+                else:
+                    print("Ingrese una opcion valida")
+        elif (option1 == 3):
+            print("Gracias por usar el programa!")
